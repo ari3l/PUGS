7a02c1675a797662b2ff9c23935674f3fe642fa7
Chaya Levin <clevin@grubhub.com>
2018-10-30 20:24:03 -0400

    fixed mapping algorithm

--- scripts/alice.py
+++ scripts/alice.py
@@ -56,6 +56,7 @@ class Alice:
         simple_list = list(string.ascii_letters + string.digits)
         symbols_list = ['-', '_', '*', '%', '!', '@', '$', '#', '^', '&']
         complex_list = list(simple_list + symbols_list)
+
         new_rwd = ''
 
         if category == 'simple':
@@ -66,12 +67,14 @@ class Alice:
         print('\nNew RWD: {0}'.format(new_rwd))
 
     def map_algorithm(self, category_list, rwd):
-        new_rwd = '' #new rwd will be of length 16 (max length)
+        new_rwd = ''
         n = 4
         split_rwd = [rwd[i:i+n] for i in range(0, len(rwd), n)]
 
-        for x in split_rwd:
-            index = int(x, 16) % len(category_list) - 1
+        for segment in split_rwd:
+            x = int(segment, 16)
+            random.seed(x)
+            index = random.randrange(0, len(category_list)-1)
             new_rwd += category_list[index]
 
         return new_rwd
