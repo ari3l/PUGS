9db6de952605ba220eddcc55584b3822066854bd
Chaya Levin <clevin@grubhub.com>
2018-11-07 16:59:34 -0500

    added one million test cases test

---
+++ tests/million_protocol_tests.py
@@ -0,0 +1,29 @@
+from scripts import clientExample
+from scripts import setup
+import random
+import string
+
+
+def main():
+    failure_counter = 0
+    success_counter = 0
+    setup.setup(100)
+    setup.main()
+    for i in range(100000):
+        print("COUNT:" + str(i))
+        try:
+            username = ''.join([random.choice(string.ascii_letters + string.digits) for n in xrange(32)])
+            password = ''.join([random.choice(string.ascii_letters + string.digits) for n in xrange(32)])
+            site = ''.join([random.choice(string.ascii_letters + string.digits) for n in xrange(32)]) + '.com'
+            category = random.choice(['simple', 'super-simple', 'complex'])
+
+            clientExample.main(username, password, site, category)
+            success_counter += 1
+        except Exception:
+            failure_counter += 1
+    print("Successes: " + str(success_counter))
+    print("Failures: " + str(failure_counter))
+
+
+if __name__ == '__main__':
+    main()
